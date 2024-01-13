d8af0603f58347cc4d6e9e97351e64f223fcf7ec
nitish-devop <devnitish97@gmail.com>
2024-01-13 13:00:15 +0530

    leetcode version : product of array except itself in O(N) Time without using division

--- Product Of Array Except itself/Implementation.java
+++ Product Of Array Except itself/Implementation.java
@@ -46,12 +46,43 @@ public class Implementation {
     }
 
 
+    // leetcode version : Don't use division operaion and try to solve in O(n) time
+    /*
+     * Idea is to use two array left and right.
+     * Left array will have product of all elements at its left.
+     * Right array will have product of all elements at its right
+     * Compute the answer array by mulitplying ith index of both array
+     * Time Complexity : O(n)
+     * Space Complexity O(n)
+     */
+
+     public static int[] approachThird(int[] nums) {
+        
+        int[] ans  = new int[nums.length];
+
+        int[] left = new int[nums.length];
+        left[0] = 1;
+        for(int i = 1; i < nums.length; i++) left[i] = left[i-1] * nums[i-1];
+
+        int[] right = new int[nums.length];
+        right[nums.length - 1] = 1;
+        for(int i = nums.length - 2; i >= 0; i--) right[i] = right[i+1] * nums[i + 1];
+
+        System.out.println(Arrays.toString(left));
+        System.out.println(Arrays.toString(right));
+        for(int i = 0; i < nums.length; i++) {
+            ans[i] = left[i] * right[i];
+        }
+        return ans;
+    }
     public static void main(String args[]) {
         int[] arr = new int[]{4, 6, 8, 2, 1};
 
         // int ans[]  = approachFirst(arr);
 
-        int ans[]  = approachSecond(arr);
+        // int ans[]  = approachSecond(arr);
+
+        int ans[] = approachThird(arr);
         System.out.println(Arrays.toString(ans));
 
     }

f71271ed1aba4e395ffd8660b1c94bf17f042a67
nitish-devop <devnitish97@gmail.com>
2024-04-27 16:47:59 +0530

    implement binary search algo

---
+++ binary_search/binary_search.js
@@ -0,0 +1,50 @@
+function binarySearchIterative(arr, target) {
+    let start = 0;
+    let end = arr.length;
+
+    while(start <= end) {
+        let mid = Math.floor((start + end) / 2);
+
+        if(arr[mid] == target) {
+            return mid;
+        }else if(arr[mid] < target) {
+            start = mid + 1;
+        }else {
+            // search in left part
+            end = mid - 1;
+        }
+    }
+    return -1
+}
+
+
+function binarySearchRecursive(arr, start, end, target) {
+   if(start > end) {
+    return -1;
+   }
+
+    let mid = Math.floor((start + end) / 2);
+
+    if(arr[mid] == target) {
+        return mid;
+    }else if(arr[mid] < target) {
+        return binarySearchRecursive(arr, mid + 1, end, target)
+    }else {
+        // search in left part
+       return binarySearchRecursive(arr, start, mid - 1, target)
+    }
+}
+
+// Example
+let arr = [3, 4, 6, 7, 9, 12, 16, 17];
+
+let target = 12;
+let ans1 = binarySearchIterative(arr, target)
+let ans2 = binarySearchRecursive(arr, 0, arr.length, target)
+console.log(ans1, ans2)
+
+target = 13;
+ ans1 = binarySearchIterative(arr, target)
+ ans2 = binarySearchRecursive(arr, 0, arr.length, target)
+console.log(ans1, ans2)
+

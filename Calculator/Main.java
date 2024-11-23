5a95aa3f387c73e98b7de48074d512242486ae98
Nitish Kumar <devnitish97@gmail.com>
2024-11-23 12:26:22 +0530

    basic command line calculator

---
+++ Calculator/Main.java
@@ -0,0 +1,9 @@
+package Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        int result = c.add(5,7);
+        System.out.println("sum is " + result);
+    }
+}
